492fc2235d73bfc8c46af7e6587a7f650abeb3f8
durga444! <160768800+Durga200331@users.noreply.github.com>
2025-06-16 23:17:26 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import VoiceDebate from "./Voicedebate";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="title">Debatox AI</h1>
+      <VoiceDebate />
+    </div>
+  );
+}
+
+export default App;
